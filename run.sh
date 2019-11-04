a0c27a9a6473e445c42e1f61043bca5d7d20cf97
Asher Spector <amspector100@gmail.com>
2019-11-04 02:22:17 -0500

    Should actually work

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #SBATCH -p janson #Partition to submit to
 #SBATCH -N 1 #Number of nodes
 #SBATCH -n 1 #Number of cores
-#SBATCH -t 0-00:05 #Runtime in (D-HH:MM)
+#SBATCH -t 0-00:10 #Runtime in (D-HH:MM)
 #SBATCH --mem-per-cpu=500 #Memory per cpu in MB (see also --mem)
 #SBATCH -o output/slurm.%N.%j.out #File to which standard out will be written
 #SBATCH -e output/slurm.%N.%j.err #File to which standard err will be written
@@ -11,11 +11,10 @@
 # Install python/anaconda, activate base environment
 module purge
 module load Anaconda3/5.0.1-fasrc02
-conda create -n knockadapt1 python=3.6
 source activate knockadapt1
 
 # Install requirements
-pip install -r requirements.txt
+# y | pip install -r requirements.txt
 python3 main.py --n 50 --p 20 --num_datasets 10 --covmethod 'ErdosRenyi' 
 module purge
 
